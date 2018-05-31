1603f372ee1a7488c70420a2e2703a6f42905fcf
js7089 <38030993+js7089@users.noreply.github.com>
2018-05-31 21:07:10 +0900

    added newproduct & search(buy)

--- jungo/views.py
+++ jungo/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
-from .forms import SignupForm, LoginForm
+from .forms import *
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as django_login
 from django.template import RequestContext
-
+from .models import *
 from django.http import HttpResponse
 
 
@@ -36,7 +36,7 @@ def login(request):
                 django_login(request, user)
                 return redirect('mainpage')
             else:
-                return HttpResponse('로그인 실패, 다시 시도 해보세요.')
+                return HttpResponse('<script> alert("Login Failed. Please Try Again"); history.back(-1);</script> ')
     
     else:
         login_form = LoginForm()
@@ -46,4 +46,40 @@ def login(request):
         return render(request, 'jungo/login.html', context)
 
 def mainpage(request):
-    return render(request, 'jungo/mainpage.html')
+    #print(User.objects.all());
+    context = {
+        'myid' : request.user.username,
+        }
+    return render(request, 'jungo/mainpage.html', context);
+    
+def productpage(request):
+    # print("Size of table <Product> : " + str(Product.objects.count())) 
+    if request.method == "POST":
+        prod_form = NewProdForm(request.POST)
+        if prod_form.is_valid():
+            prod_form.create()
+            return redirect('mainpage')
+    else:
+        prod_form = NewProdForm()
+        context = {
+            'prod_form': prod_form,
+            'seller_id': request.user.username,
+        }
+        return render(request, 'jungo/product.html', context)
+
+def searchpage_buy(request):
+    if request.method == "POST":
+        search_form = SearchField(request.POST)
+        if search_form.is_valid():
+            posts=search_form.find()
+            context = {
+                'search_form' : search_form,
+                'posts' : posts,
+                }
+            return render(request, 'jungo/search.html', context)
+    else:
+        search_form = SearchField()
+        context = {
+            'search_form': search_form,
+        }
+        return render(request, 'jungo/search.html', context)
