458dd619d922a682de15b037e43016021846e099
js7089 <38030993+js7089@users.noreply.github.com>
2018-05-31 21:06:26 +0900

    added product/search form
    new product & search product(from table <product>)

--- jungo/forms.py
+++ jungo/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from django.contrib.auth.models import User
+from django.contrib.auth.models import *
+from jungo.choices import *
+from jungo.models import *
 
 class SignupForm(forms.Form):
     username = forms.CharField(
@@ -40,3 +42,70 @@ class LoginForm(forms.Form):
             }
         )
     )
+
+
+class NewProdForm(forms.Form):
+    prodName = forms.CharField(
+    widget = forms.TextInput(
+        attrs={
+            'class': 'form-control',
+              }
+        )
+    )
+    prodtype = forms.ChoiceField(label="",
+                                initial='',
+                                widget=forms.Select(),
+                                required=True, choices=STATUS_CHOICES)
+    price = forms.CharField(
+        widget=forms.TextInput(
+            attrs={ 'class':'form-control',}
+            )
+        )
+    prodDesc = forms.CharField(
+        widget=forms.TextInput(
+            attrs={ 'class':'form-control',}
+            )
+        )
+
+    def create(self):
+        if self.is_valid():
+            return Product.objects.create(
+                pid=Product.objects.count(),    
+                prodName=self.cleaned_data['prodName'],
+                prodType=self.cleaned_data['prodtype'],
+                price=self.cleaned_data['price'],
+                prodDesc=self.cleaned_data['prodDesc']
+                )
+
+class SearchField(forms.Form):
+    nameFilter = forms.CharField(required=False,
+    widget = forms.TextInput(
+        attrs={
+            'class': 'form-control',
+              }
+        )
+    )
+    typeFilter = forms.ChoiceField(label="",
+                                initial='',
+                                widget=forms.Select(),
+                                required=True, choices=STATUS_CHOICES_B)
+    priceLo = forms.CharField(
+        widget=forms.TextInput(
+            attrs={ 'class':'form-control',}
+            )
+        )
+    priceHi = forms.CharField(
+        widget=forms.TextInput(
+            attrs={ 'class':'form-control',}
+            )
+        )
+
+    def find(self):
+        if self.is_valid():
+            qset = Product.objects.filter(
+                price__lte=self.cleaned_data['priceHi'],
+                price__gte=self.cleaned_data['priceLo'],
+                prodName__contains=self.cleaned_data['nameFilter'],
+                prodType=self.cleaned_data['typeFilter']
+                );
+            return qset;
