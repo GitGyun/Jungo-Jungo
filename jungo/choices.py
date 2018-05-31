d600009c33fd9cb897f9812f35c5b8163e42c4cd
js7089 <38030993+js7089@users.noreply.github.com>
2018-05-31 21:07:49 +0900

    Add files via upload

---
+++ jungo/choices.py
@@ -0,0 +1,14 @@
+STATUS_CHOICES = (
+    ('A', 'Appliances'),
+    ('B', 'Textbooks'),
+    ('C', 'Vehicles'),
+    ('D', 'Others'),
+)
+
+STATUS_CHOICES_B = (
+    ('0', 'All'),    
+    ('A', 'Appliances'),
+    ('B', 'Textbooks'),
+    ('C', 'Vehicles'),
+    ('D', 'Others'),
+)
